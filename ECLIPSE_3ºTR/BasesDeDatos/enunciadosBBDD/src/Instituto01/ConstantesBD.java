11c6f2eef154f85c2b783c52b2eedabca64d54a7
garchyLinux <jandro.garcia.sanchez@gmail.com>
2024-03-13 18:38:37 +0100

    Tras el head mal

--- ECLIPSE_3ºTR/BasesDeDatos/enunciadosBBDD/src/Instituto01/ConstantesBD.java
+++
@@ -1,7 +0,0 @@
-package Instituto01;
-
-public class ConstantesBD {
-	public static final String URL="jdbc:mysql://localhost/instituto";
-	public static final String USUARIO="root";
-	public static final String PWD="";
-}
